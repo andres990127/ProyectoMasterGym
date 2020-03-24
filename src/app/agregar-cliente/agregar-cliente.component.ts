fe5f34f9c4cf9b115183a8cad2f5373745753dd3
Unear <andres_mau.gutierrez@uao.edu.co>
2020-03-24 10:44:11 -0500

    Version validad formulario agregar cliente

--- src/app/agregar-cliente/agregar-cliente.component.ts
+++ src/app/agregar-cliente/agregar-cliente.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-agregar-cliente',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AgregarClienteComponent implements OnInit {
 
-  constructor() { }
+  formularioCliente: FormGroup; /* Creamos el nuevo formulario para ingresar al cliente */
+  constructor(private fb: FormBuilder) { } /* Inyectamos el servicio de formularios */
 
   ngOnInit() {
+    /*         Codigo para validar formulario */
+    this.formularioCliente = this.fb.group({
+      nombre: ['', Validators.required],   /* Validators.required significa que el campo es requerido */
+      apellido: ['', Validators.required],
+      correo: ['', Validators.compose([    /* Validators.compose significa que el campo tiene varios requerimientos, dentro se colocan todos los requerimientos necesarios */
+        Validators.required, Validators.email
+      ])],
+      cedula: [''],
+      fechaNacimiento: ['', Validators.required],
+      telefono: [''],
+      imgUrl: ['', Validators.required]
+    })
+    /*         Fin codigo para validar formulario */
+
+  }
+
+  agregar()
+  {
+    console.log(this.formularioCliente.value)
   }
 
 }
