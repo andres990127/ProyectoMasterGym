114c76794813a4b195e4edc709cb67346029d871
Unear <andres_mau.gutierrez@uao.edu.co>
2020-03-28 17:33:08 -0500

    Version Output

--- src/app/inscripcion/inscripcion.component.ts
+++ src/app/inscripcion/inscripcion.component.ts
@@ -8,26 +8,26 @@ import { Cliente } from '../models/cliente';
   styleUrls: ['./inscripcion.component.scss']
 })
 export class InscripcionComponent implements OnInit {
-  inscripcion: Inscripcion = new Inscripcion();
-  clienteSeleccionado: Cliente = new Cliente();
+  inscripcion: Inscripcion = new Inscripcion(); /* Creo una nueva inscripcion que guardara la informacion de inscripcion */
+  clienteSeleccionado: Cliente = new Cliente(); /* Creo una variable que contendrá al usuario seleccionado para la inscripcion */
   constructor() { }
 
   ngOnInit() {
   }
 
-  asignarCliente(cliente: Cliente)
+  asignarCliente(cliente: Cliente) /* Funcion para asignar el cliente y la inscripcion */
   {
-    this.inscripcion.cliente = cliente.ref
-    this.clienteSeleccionado = cliente;
+    this.inscripcion.cliente = cliente.ref /* Asigno a la variable 'cliente' de inscripcion la informacion recuperada */
+    this.clienteSeleccionado = cliente; /* Asigno a la variable local clienteSeleccionado la informacion del cliente recuperada */
   }
 
-  eliminarCliente()
+  eliminarCliente() /* Funcion para eliminar el cliente y la inscripcion actual */
   {
     this.clienteSeleccionado = new Cliente();
     this.inscripcion.cliente = undefined;
   }
 
-  guardar(){
+  guardar(){ /* Funcion para subir a la BD la inscripcion y el cliente de este */
     console.log(this.inscripcion);
   }
 }
